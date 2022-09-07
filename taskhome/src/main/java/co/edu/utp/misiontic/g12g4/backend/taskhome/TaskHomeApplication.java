808cff770b6863fe5a1bb945aab4f8fb4b7d5064
Elkin Ocampo <elkinocampo1053@gmail.com>
2022-09-06 23:42:35 -0500

    Se inicia con la pantalla de bienvenida de la aplicacion

---
+++ taskhome/src/main/java/co/edu/utp/misiontic/g12g4/backend/taskhome/TaskHomeApplication.java
@@ -0,0 +1,13 @@
+package co.edu.utp.misiontic.g12g4.backend.taskhome;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskHomeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskHomeApplication.class, args);
+	}
+
+}
---
+++ taskhome/src/test/java/co/edu/utp/misiontic/g12g4/backend/taskhome/TaskHomeApplicationTests.java
@@ -0,0 +1,13 @@
+package co.edu.utp.misiontic.g12g4.backend.taskhome;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskHomeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
